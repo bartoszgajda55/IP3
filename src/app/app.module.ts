88ec0a53f79dd56e46129d5f2b1ab8ad3b92fd0b
Bartosz Gajda <bg@bartoszgajda.com>
2019-02-16 13:42:42 +0000

    Clean up root app files. Move router outlet to nav component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SideNavComponent } from "./components/side-nav/side-nav.component";
+import { MaterialModule } from "./modules/material/material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SideNavComponent
-  ],
+  declarations: [AppComponent, SideNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
