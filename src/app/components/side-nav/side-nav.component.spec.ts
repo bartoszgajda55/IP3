cdc197541d37a2e71b26a418792483fd192e3acd
Bartosz Gajda <bg@bartoszgajda.com>
2019-02-16 14:42:10 +0000

    Fix failing tests.

--- src/app/components/side-nav/side-nav.component.spec.ts
+++ src/app/components/side-nav/side-nav.component.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { AppModule } from "../../app.module";
+import { SideNavComponent } from "./side-nav.component";
 
-import { SideNavComponent } from './side-nav.component';
-
-describe('SideNavComponent', () => {
+describe("SideNavComponent", () => {
   let component: SideNavComponent;
   let fixture: ComponentFixture<SideNavComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SideNavComponent ]
-    })
-    .compileComponents();
+      imports: [AppModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('SideNavComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
