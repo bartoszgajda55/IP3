bbf210e773c4d870069c500d58071718603cc8ed
Bartosz Gajda <bg@bartoszgajda.com>
2019-02-17 13:00:54 +0000

    Generate interface for Navigation Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  { path: "", component: PageNotFoundComponent, pathMatch: "full" },
   { path: "weather", component: PageNotFoundComponent },
   { path: "earthquakes", component: PageNotFoundComponent },
   { path: "authors", component: PageNotFoundComponent },
   { path: "tutorial", component: PageNotFoundComponent },
+  { path: "", component: PageNotFoundComponent, pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent }
 ];
 
