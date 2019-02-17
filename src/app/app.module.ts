3f910d6b94d136054f8054b508097e2e28dd32cf
Bartosz Gajda <bg@bartoszgajda.com>
2019-02-17 12:45:17 +0000

    Add Page Not Found component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
 import { MaterialModule } from "./modules/material/material.module";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent],
+  declarations: [AppComponent, SideNavComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
