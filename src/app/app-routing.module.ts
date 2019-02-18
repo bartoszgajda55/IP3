252c49eb08c22ba05138aafcfad3bf71440827d0
Bartosz Gajda <bg@bartoszgajda.com>
2019-02-18 16:51:06 +0000

    Add weather component. Update route for it.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { WeatherComponent } from './components/weather/weather.component';
 
 const routes: Routes = [
-  { path: "weather", component: PageNotFoundComponent },
+  { path: "weather", component: WeatherComponent },
   { path: "earthquakes", component: PageNotFoundComponent },
   { path: "authors", component: PageNotFoundComponent },
   { path: "tutorial", component: PageNotFoundComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
 import { MaterialModule } from "./modules/material/material.module";
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent, PageNotFoundComponent],
+  declarations: [AppComponent, SideNavComponent, PageNotFoundComponent, WeatherComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
