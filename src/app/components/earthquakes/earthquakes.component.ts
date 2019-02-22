ac401a55793c6f998f2062cb754122973d7a71a5
hawrot <mateusz@sent.com>
2019-02-22 11:43:31 +0000

    Add earthquake component

---
+++ src/app/components/earthquakes/earthquakes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-earthquakes',
+  templateUrl: './earthquakes.component.html',
+  styleUrls: ['./earthquakes.component.scss']
+})
+export class EarthquakesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
