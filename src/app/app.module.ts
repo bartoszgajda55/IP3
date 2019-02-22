88fd55b1d245593f631a9bba232baedff0ca604a
hawrot <mateusz@sent.com>
2019-02-22 11:32:44 +0000

    Add earthquake compoment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
 import { MaterialModule } from "./modules/material/material.module";
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { EarthquakeComponent } from './components/earthquake/earthquake.component';
 
 @NgModule({
-  declarations: [AppComponent, SideNavComponent, PageNotFoundComponent],
+  declarations: [AppComponent, SideNavComponent, PageNotFoundComponent, EarthquakeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/earthquake/earthquake.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-earthquake',
+  templateUrl: './earthquake.component.html',
+  styleUrls: ['./earthquake.component.scss']
+})
+export class EarthquakeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
