8f856aa26037226aed6ce811e8d0fe60bd3d8774
Bartosz Gajda <bg@bartoszgajda.com>
2019-02-23 18:09:58 +0000

    Clean up weather-page component.

--- src/app/components/weather-page/weather-page.component.ts
+++ src/app/components/weather-page/weather-page.component.ts
@@ -24,17 +24,21 @@ export class WeatherPageComponent {
   @ViewChild("search")
   public searchElementRef: ElementRef;
 
-  constructor(
-    private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone,
-    private weatherService: OpenWeatherService,
-    private weatherIconService: WeatherIconService
-  ) {}
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone, private weatherService: OpenWeatherService) {}
 
   ngOnInit() {
     this.initializeFields();
     this.setDefaultPosition();
     this.attachListenerToSearchBox();
+  }
+
+  private initializeFields(): void {
+    this.zoom = 12;
+    this.lat = 55.873543;
+    this.lng = -4.289058;
+    this.searchControl = new FormControl();
+    this.currentLocationName = "Glasgow, UK";
+    this.currentWeather$ = this.fetchCurrentWeatherByLocation(this.lat, this.lng);
   }
 
   private setDefaultPosition(): void {
@@ -46,13 +50,17 @@ export class WeatherPageComponent {
     }
   }
 
-  private initializeFields(): void {
-    this.zoom = 12;
-    this.lat = 55.873543;
-    this.lng = -4.289058;
-    this.searchControl = new FormControl();
-    this.currentLocationName = "Glasgow, UK";
-    this.currentWeather$ = this.fetchCurrentWeatherByLocation(this.lat, this.lng);
+  private attachListenerToSearchBox(): void {
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["(cities)"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          this.updateMapWithPlace(autocomplete);
+        });
+      });
+    });
   }
 
   private updateMapWithPlace(parent: google.maps.places.Autocomplete): void {
@@ -68,18 +76,5 @@ export class WeatherPageComponent {
 
   private fetchCurrentWeatherByLocation(lat: number, lng: number): Observable<CurrentWeather> {
     return this.weatherService.getCurrentWeatherByCoordinates(lat, lng);
-  }
-
-  private attachListenerToSearchBox(): void {
-    this.mapsAPILoader.load().then(() => {
-      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-        types: ["(cities)"]
-      });
-      autocomplete.addListener("place_changed", () => {
-        this.ngZone.run(() => {
-          this.updateMapWithPlace(autocomplete);
-        });
-      });
-    });
   }
 }
