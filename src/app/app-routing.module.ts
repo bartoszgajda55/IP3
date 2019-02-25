27001d2ca943dc66c7e110d5960aec801f491df6
HafsahKamran <hkamra200@caledonian.ac.uk>
2019-02-25 13:43:39 +0000

    Credentials fix.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { AuthorsPageComponent } from './components/authors-page/authors-page.component';
 
 const routes: Routes = [
   { path: "weather", component: PageNotFoundComponent },
   { path: "earthquakes", component: PageNotFoundComponent },
-  { path: "authors", component: PageNotFoundComponent },
+  { path: "authors", component: AuthorsPageComponent },
   { path: "tutorial", component: PageNotFoundComponent },
   { path: "", component: PageNotFoundComponent, pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent }
