67586bcbc5c0087452d327f9c94396484b0a53f1
hawrot <mhawro200@caledonian.ac.uk>
2019-03-03 18:54:15 +0000

    Add earthquake service

---
+++ src/app/services/earthquake/earthquake.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EarthquakeService {
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getEarthquakes(): Observable<any> {
+    return this.http.get('https://cors-anywhere.herokuapp.com/http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
+      .pipe(map(data => data));
+
+
+  }
+
+}
