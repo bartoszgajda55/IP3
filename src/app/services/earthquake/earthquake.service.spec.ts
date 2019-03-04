b72556883b25a52cc826cb0a86711ec5b29423a4
hawrot <mhawro200@caledonian.ac.uk>
2019-03-04 19:59:47 +0000

    Fix tests

--- src/app/services/earthquake/earthquake.service.spec.ts
+++ src/app/services/earthquake/earthquake.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EarthquakeService } from './earthquake.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('EarthquakeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: EarthquakeService = TestBed.get(EarthquakeService);
