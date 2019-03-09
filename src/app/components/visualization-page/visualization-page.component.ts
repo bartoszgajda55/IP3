9181e60b7dbd3b6952a8cff9ddc57e5084f038d2
Bartosz Gajda <bg@bartoszgajda.com>
2019-03-09 21:27:14 +0000

    Displayed google map. Need to add layer to it.

--- src/app/components/visualization-page/visualization-page.component.ts
+++ src/app/components/visualization-page/visualization-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, OnInit } from "@angular/core";
+import { MapsAPILoader, GoogleMapsAPIWrapper } from "@agm/core";
 
 @Component({
-  selector: 'app-visualization-page',
-  templateUrl: './visualization-page.component.html',
-  styleUrls: ['./visualization-page.component.scss']
+  selector: "app-visualization-page",
+  templateUrl: "./visualization-page.component.html",
+  styleUrls: ["./visualization-page.component.scss"]
 })
 export class VisualizationPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private googleMapsApiWrapper: GoogleMapsAPIWrapper) {}
 
   ngOnInit() {
+    this.googleMapsApiWrapper.getNativeMap().then(map => {});
   }
-
 }
