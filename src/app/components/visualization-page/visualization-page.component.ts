4f7c0c30179861d7f721c721560fc862922606e7
Bartosz Gajda <bg@bartoszgajda.com>
2019-03-10 16:49:25 +0000

    Add displaying current layer name in template.

--- src/app/components/visualization-page/visualization-page.component.ts
+++ src/app/components/visualization-page/visualization-page.component.ts
@@ -13,24 +13,29 @@ import {} from "googlemaps";
 })
 export class VisualizationPageComponent {
   private map: any;
-  private defaultLayerName: string = "TA2";
+  private layerCode: string;
+  public layerName: string;
   public availableMapLayers: any[] = layers;
   public mapStyles = styles;
 
   public onMapReady(map): void {
+    this.layerCode = this.availableMapLayers[0].value;
+    this.layerName = this.availableMapLayers[0].name;
     this.map = map;
-    this.setMapWithLayer(this.defaultLayerName, this.map);
+    this.setMapWithLayer(this.layerCode, this.map);
   }
 
   public handleUserMapLayerSelection(event: MatSelectChange): void {
-    this.setMapWithLayer(event.value, this.map);
+    this.layerCode = event.value;
+    this.layerName = event.source.triggerValue;
+    this.setMapWithLayer(this.layerCode, this.map);
   }
 
-  private setMapWithLayer(layerName: string, map: any): void {
+  private setMapWithLayer(layerCode: string, map: any): void {
     map.overlayMapTypes.clear();
     var imageMapType = new google.maps.ImageMapType({
       getTileUrl: function(coord, zoom) {
-        return `https://a.sat.owm.io/vane/2.0/weather/${layerName}/${zoom}/${coord.x}/${coord.y}?appid=9de243494c0b295cca9337e1e96b00e2`;
+        return `https://a.sat.owm.io/vane/2.0/weather/${layerCode}/${zoom}/${coord.x}/${coord.y}?appid=9de243494c0b295cca9337e1e96b00e2`;
       },
       tileSize: new google.maps.Size(256, 256)
     });
