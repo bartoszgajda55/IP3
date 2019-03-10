c434bd16335df6ff5e11e7089e68181ab5e00c04
Bartosz Gajda <bg@bartoszgajda.com>
2019-03-10 15:40:21 +0000

    Add layer drawing to google map.

--- src/app/components/visualization-page/visualization-page.component.ts
+++ src/app/components/visualization-page/visualization-page.component.ts
@@ -1,6 +1,6 @@
 /// <reference types="@types/googlemaps" />
 import { Component, OnInit } from "@angular/core";
-import { GoogleMapsAPIWrapper } from "@agm/core";
+import styles from "../../data/map-style";
 import {} from "googlemaps";
 
 @Component({
@@ -9,36 +9,19 @@ import {} from "googlemaps";
   styleUrls: ["./visualization-page.component.scss"]
 })
 export class VisualizationPageComponent implements OnInit {
-  constructor(private googleMapsApiWrapper: GoogleMapsAPIWrapper) {}
+  public mapStyles = styles;
+  constructor() {}
 
-  ngOnInit() {
-    this.googleMapsApiWrapper.getNativeMap().then((map: any) => {
-      var bounds = {
-        17: [[20969, 20970], [50657, 50658]],
-        18: [[41939, 41940], [101315, 101317]],
-        19: [[83878, 83881], [202631, 202634]],
-        20: [[167757, 167763], [405263, 405269]]
-      };
+  ngOnInit() {}
 
-      var imageMapType = new google.maps.ImageMapType({
-        getTileUrl: function(coord, zoom) {
-          if (
-            zoom < 17 ||
-            zoom > 20 ||
-            bounds[zoom][0][0] > coord.x ||
-            coord.x > bounds[zoom][0][1] ||
-            bounds[zoom][1][0] > coord.y ||
-            coord.y > bounds[zoom][1][1]
-          ) {
-            return null;
-          }
-
-          return ["//www.gstatic.com/io2010maps/tiles/5/L2_", zoom, "_", coord.x, "_", coord.y, ".png"].join("");
-        },
-        tileSize: new google.maps.Size(256, 256)
-      });
-
-      map.overlayMapTypes.push(imageMapType);
+  public onMapReady(map): void {
+    var imageMapType = new google.maps.ImageMapType({
+      getTileUrl: function(coord, zoom) {
+        return ["https://a.sat.owm.io/vane/2.0/weather/TA2/", zoom, "/", coord.x, "/", coord.y, "?appid=9de243494c0b295cca9337e1e96b00e2"].join("");
+      },
+      tileSize: new google.maps.Size(256, 256)
     });
+
+    map.overlayMapTypes.push(imageMapType);
   }
 }
