ccc902f6368227f65aa9e5561ec26a831238105c
Bartosz <bg@bartoszgajda.com>
2019-03-24 00:31:34 +0000

    Clean up earthquake service.

--- src/app/services/earthquake/earthquake.service.ts
+++ src/app/services/earthquake/earthquake.service.ts
@@ -1,37 +1,25 @@
-import {Injectable} from '@angular/core';
-import {HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EarthquakeService {
-
-
+  private apiUri: string;
   constructor(private http: HttpClient) {
+    this.apiUri = "https://cors-anywhere.herokuapp.com/http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary";
   }
 
   public getEarthquakesAllDay(): Observable<any> {
-    return this.http.get('https://cors-anywhere.herokuapp.com/http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson')
-      .pipe(map(data => data));
-
-
+    return this.http.get(`${this.apiUri}/all_day.geojson`);
   }
   public getEarthquakesLastHour(): Observable<any> {
-    return this.http.get('https://cors-anywhere.herokuapp.com/http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
-      .pipe(map(data => data));
-
-
+    return this.http.get(`${this.apiUri}/all_hour.geojson`);
   }
 
   public getEarthquakesallMonth(): Observable<any> {
-    return this.http.get('https://cors-anywhere.herokuapp.com/http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson')
-      .pipe(map(data => data));
-
-
+    return this.http.get(`${this.apiUri}/all_month.geojson`);
   }
-
 }
