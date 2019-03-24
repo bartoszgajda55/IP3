2ae33837791bd134c265823d0d6449c17b611232
Bartosz <bg@bartoszgajda.com>
2019-03-24 12:13:54 +0000

    Update ts file for earthquake page.

--- src/app/components/earthquake-page/earthquake-page.component.ts
+++ src/app/components/earthquake-page/earthquake-page.component.ts
@@ -1,34 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-
-import {EarthquakeService} from '../../services/earthquake/earthquake.service';
-import {Observable} from 'rxjs';
-import { MatProgressSpinnerModule } from '@angular/material';
-
-
+/// <reference types="@types/googlemaps" />
+import { Component } from "@angular/core";
+import { EarthquakeService } from "../../services/earthquake/earthquake.service";
+import { Observable } from "rxjs";
+import { MatSelectChange } from "@angular/material";
+import { FeatureCollection, Feature } from "geojson";
+import { take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-earthquake-page',
-  templateUrl: './earthquake-page.component.html',
-  styleUrls: ['./earthquake-page.component.scss']
+  selector: "app-earthquake-page",
+  templateUrl: "./earthquake-page.component.html",
+  styleUrls: ["./earthquake-page.component.scss"]
 })
-export class EarthquakePageComponent implements OnInit {
+export class EarthquakePageComponent {
+  public markers: Array<Feature>;
 
-  selected = 'option1';
-  public earthquakes = []
-  public currentEarthquakes$: Observable<any>;
-  public LastHourEarthquakes$: Observable<any>;
-  public AllMonthEarthquakes$: Observable<any>;
+  constructor(private earthquakeService: EarthquakeService) {}
 
-
-  constructor(private earthquakeService: EarthquakeService) {
-
+  public onMapReady(map): void {
+    this.setMarkersFromAllDay();
   }
 
-  ngOnInit() {
-    this.currentEarthquakes$ = this.earthquakeService.getEarthquakesAllDay();
-    this.LastHourEarthquakes$ = this.earthquakeService.getEarthquakesLastHour();
-    this.AllMonthEarthquakes$ = this.earthquakeService.getEarthquakesallMonth();
+  onTimeFrameChange(event: MatSelectChange): void {
+    this.handleTimeFrameChange(event.value);
+  }
+
+  private handleTimeFrameChange(timeFrame: string): void {
+    switch (timeFrame) {
+      case "hour": {
+        this.setMarkersFromLastHour();
+        break;
+      }
+      case "day": {
+        this.setMarkersFromAllDay();
+        break;
+      }
+      case "month": {
+        this.setMarkersFromAllMonth();
+        break;
+      }
+      default: {
+        this.setMarkersFromAllDay();
+        break;
+      }
+    }
+  }
+
+  private setMarkersFromLastHour(): void {
+    this.earthquakeService
+      .getEarthquakesLastHour()
+      .pipe(take(1))
+      .subscribe(data => {
+        this.markers = data.features;
+      });
+  }
+
+  private setMarkersFromAllDay(): void {
+    this.earthquakeService
+      .getEarthquakesAllDay()
+      .pipe(take(1))
+      .subscribe(data => {
+        this.markers = data.features;
+      });
+  }
+
+  private setMarkersFromAllMonth(): void {
+    this.earthquakeService
+      .getEarthquakesAllMonth()
+      .pipe(take(1))
+      .subscribe(data => {
+        this.markers = data.features;
+      });
   }
 }
-
-
