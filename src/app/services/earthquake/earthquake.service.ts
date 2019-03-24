59b781f759e45af1d2cd507dd8dc0fe89c25a62a
Bartosz <bg@bartoszgajda.com>
2019-03-24 12:12:19 +0000

    Fix typo in earthquake service.

--- src/app/services/earthquake/earthquake.service.ts
+++ src/app/services/earthquake/earthquake.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { FeatureCollection } from "geojson";
 
 @Injectable({
   providedIn: "root"
@@ -11,14 +12,14 @@ export class EarthquakeService {
     this.apiUri = "https://cors-anywhere.herokuapp.com/http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary";
   }
 
-  public getEarthquakesAllDay(): Observable<any> {
-    return this.http.get(`${this.apiUri}/all_day.geojson`);
+  public getEarthquakesAllDay(): Observable<FeatureCollection> {
+    return this.http.get<FeatureCollection>(`${this.apiUri}/all_day.geojson`);
   }
-  public getEarthquakesLastHour(): Observable<any> {
-    return this.http.get(`${this.apiUri}/all_hour.geojson`);
+  public getEarthquakesLastHour(): Observable<FeatureCollection> {
+    return this.http.get<FeatureCollection>(`${this.apiUri}/all_hour.geojson`);
   }
 
-  public getEarthquakesallMonth(): Observable<any> {
-    return this.http.get(`${this.apiUri}/all_month.geojson`);
+  public getEarthquakesAllMonth(): Observable<FeatureCollection> {
+    return this.http.get<FeatureCollection>(`${this.apiUri}/all_month.geojson`);
   }
 }
