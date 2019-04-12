05d8ed9bee1e6eda2e03083d824d0dce703414f8
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-12 17:46:52 +0100

    Center tab content and image.

--- src/app/components/js-techniques/js-techniques.component.ts
+++ src/app/components/js-techniques/js-techniques.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'app-js-techniques',
-  templateUrl: './js-techniques.component.html',
-  styleUrls: ['./js-techniques.component.scss']
+  selector: "app-js-techniques",
+  templateUrl: "./js-techniques.component.html",
+  styleUrls: ["./js-techniques.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class JsTechniquesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
