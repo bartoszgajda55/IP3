79f10877605ee4281dd5a446769ce85382cc76a4
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-15 09:57:14 +0100

    Clean up template and styles. Fix test.

--- src/app/components/earthquake-tutorial/earthquake-tutorial.component.spec.ts
+++ src/app/components/earthquake-tutorial/earthquake-tutorial.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { EarthquakeTutorialComponent } from './earthquake-tutorial.component';
+import { EarthquakeTutorialComponent } from "./earthquake-tutorial.component";
+import { AppComponent } from "src/app/app.component";
 
-describe('EarthquakeTutorialComponent', () => {
+describe("EarthquakeTutorialComponent", () => {
   let component: EarthquakeTutorialComponent;
   let fixture: ComponentFixture<EarthquakeTutorialComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EarthquakeTutorialComponent ]
-    })
-    .compileComponents();
+      imports: [AppComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +19,7 @@ describe('EarthquakeTutorialComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
