9922bd6865cfd134fc2b82982fbba37ac2287a6e
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-22 15:50:22 +0100

    Add description to every map layer.

--- src/app/data/map-layers.ts
+++ src/app/data/map-layers.ts
@@ -7,58 +7,86 @@ export default [
   },
   {
     name: "Precipitation intensity",
-    value: "PR0"
+    value: "PR0",
+    description:
+      "The intensity of precipitation is a measure of the amount of rain and snow that falls over time. The intensity of precipitation is measured in the height of the water layer covering the ground in a period of time. It means that if the rain or snow stays where it falls, it would form a layer of a certain height. The darker the color on the map, the larger precipitation is."
   },
   {
     name: "Accumulated precipitation",
-    value: "PA0"
+    value: "PA0",
+    description:
+      "Accumulated precipitation is a measure of the amount of rain and snow that falls over one day. The intensity of precipitation is measured in the height of the water layer covering the ground in a period of time. It means that if the rain or snow stays where it falls, it would form a layer of a certain height. The darker the color on the map, the larger precipitation is."
   },
   {
     name: "Accumulated precipitation - rain",
-    value: "PAR0"
+    value: "PAR0",
+    description:
+      "Accumulated precipitation - rain is a measure of the amount of rain that falls over time. The intensity of rain is measured in the height of the water layer covering the ground in a period of time. It means that if the rain stays where it falls, it would form a layer of a certain height. The darker the color on the map, the larger rainfall is."
   },
   {
     name: "Accumulated precipitation - snow",
-    value: "PAS0"
+    value: "PAS0",
+    description:
+      "Accumulated precipitation - snow is a measure of the amount of snow that falls over time. The intensity of snow is measured in the height of the snow layer covering the ground in a period of time. It means that if the snow stays where it falls, it would form a layer of a certain height. The darker the color on the map, the larger snowfall is."
   },
   {
     name: "Depth of snow",
-    value: "SD0"
+    value: "SD0",
+    description:
+      "Snow accumulates from a series of snow events, punctuated by freezing and thawing, over areas that are cold enough to retain snow seasonally or perennially. The darker the color on the map, the deeper the layer of the snow is."
   },
   {
     name: "Wind speed at an altitude of 10 meters",
-    value: "WS10"
+    value: "WS10",
+    description:
+      "Wind speed, or wind flow velocity, is a fundamental atmospheric quantity caused by air moving from high to low pressure, usually due to changes in temperature. Note that wind direction is usually almost parallel to isobars (and not perpendicular, as one might expect), due to Earth's rotation. The darker the line, the stronger the wind is."
   },
   {
     name: "Joint display of speed wind (color) and wind direction (arrows), received by U and V components",
-    value: "WND"
+    value: "WND",
+    description:
+      "Wind direction is reported by the direction from which it originates. For example, a northerly wind blows from the north to the south.[1] Wind direction is usually reported in cardinal directions or in azimuth degrees. Wind direction is measured in degrees clockwise from due north."
   },
   {
     name: "Atmospheric pressure on mean sea level",
-    value: "APM"
+    value: "APM",
+    description:
+      "Atmospheric pressure, sometimes also called barometric pressure (after the sensor), is the pressure within the atmosphere of Earth. The colder the color, the lower the pressure is."
   },
   {
     name: "Air temperature at a height of 2 meters",
-    value: "TA2"
+    value: "TA2",
+    description:
+      "Atmospheric temperature is a measure of temperature at different levels of the Earth's atmosphere. It is governed by many factors, including incoming solar radiation, humidity and altitude. The colder the color, the lower the temperature is."
   },
   {
     name: "Temperature of a dew point",
-    value: "TD2"
+    value: "TD2",
+    description:
+      "The dew point is the temperature to which air must be cooled to become saturated with water vapor. When further cooled, the airborne water vapor will condense to form liquid water (dew). The colder the color, the lower the dew point is."
   },
   {
     name: "Soil temperature 0-10 сm",
-    value: "TS0"
+    value: "TS0",
+    description:
+      "Soil temperature depends on the ratio of the energy absorbed to that lost. Soil has a temperature range between -20 to 60 °C, with a mean annual temperature from -10 to 26 °C according to biomes. Soil temperature regulates seed germination, breaking of seed dormancy, plant and root growth and the availability of nutrients. The colder the color, the lower the temperature is."
   },
   {
     name: "Soil temperature >10 сm",
-    value: "TS10"
+    value: "TS10",
+    description:
+      "Soil temperature depends on the ratio of the energy absorbed to that lost. Soil has a temperature range between -20 to 60 °C, with a mean annual temperature from -10 to 26 °C according to biomes. Soil temperature regulates seed germination, breaking of seed dormancy, plant and root growth and the availability of nutrients. The colder the color, the lower the temperature is."
   },
   {
     name: "Relative humidity",
-    value: "HRD0"
+    value: "HRD0",
+    description:
+      "Humidity is the amount of water vapour present in air. Water vapour, the gaseous state of water, is generally invisible to the human eye. Humidity indicates the likelihood for precipitation, dew, or fog to be present. The amount of water vapour needed to achieve saturation increases as the temperature increases. The colder the color, the higher the humidity is."
   },
   {
     name: "Cloudiness",
-    value: "CL"
+    value: "CL",
+    description:
+      "Cloudiness refers to the fraction of the sky obscured by clouds when observed from a particular location. Okta is the usual unit of measurement of the cloud cover. The cloud cover is correlated to the sunshine duration as the least cloudy locales are the sunniest ones while the cloudiest areas are the least sunny places. The darker the line, the deeper clouds are."
   }
 ];
